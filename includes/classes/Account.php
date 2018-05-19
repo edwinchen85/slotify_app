564fe77c892424946ef93e58e332f7538d44ac3c
Edwin Chen <edwinchen85@gmail.com>
2018-05-19 20:13:21 +0800

    Pass arguments into Account register function
    Correct

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -5,12 +5,13 @@
 
     }
 
-    public function register() {
-      $this->validateUsername($username);
-      $this->validateFirstName($firstName);
-      $this->validateLastName($lastName);
-      $this->validateEmails($email, $email2);
-      $this->validatePasswords($password, $password2);
+    public function register($un, $fn, $ln, $em, $em2, $pw,
+    $pw2) {
+      $this->validateUsername($un);
+      $this->validateFirstName($fn);
+      $this->validateLastName($ln);
+      $this->validateEmails($em, $em2);
+      $this->validatePasswords($pw, $pw2);
     }
 
     private function validateUsername($un) {
