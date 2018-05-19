a4beb1809da3fe6a0dadfbac46f70de4d572f5a3
Edwin Chen <edwinchen85@gmail.com>
2018-05-19 20:05:36 +0800

    Create Account class

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -18,26 +18,6 @@ function sanitizeFormString($inputText) {
   return $inputText;
 }
 
-function validateUsername($un) {
-
-}
-
-function validateFirstName($fn) {
-
-}
-
-function validateLastName($ln) {
-
-}
-
-function validateEmails($em, $em2) {
-
-}
-
-function validatePasswords($pw, $pw2) {
-
-}
-
 if (isset($_POST['registerButton'])) {
   // Register button was pressed
   $username = sanitizeFormUserName($_POST['username']);
@@ -47,12 +27,6 @@ if (isset($_POST['registerButton'])) {
   $email2 = sanitizeFormString($_POST['email2']);
   $password = sanitizeFormPassword($_POST['password']);
   $password2 = sanitizeFormPassword($_POST['password2']);
-
-  validateUsername($username);
-  validateFirstName($firstName);
-  validateLastName($lastName);
-  validateEmails($email, $email2);
-  validatePasswords($password, $password2);
 }
 
 ?>
