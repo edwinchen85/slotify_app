1c01085ac6bf8abdf589aa87a8e9ea3828d793e4
Edwin Chen <edwinchen85@gmail.com>
2018-05-20 18:01:02 +0800

    Organize code into header.php and footer.php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+        </div>
+
+      </div>
+
+    </div>
+
+    <div id="nowPlayingBarContainer">
+
+      <?php include("includes/nowPlayingBar.php"); ?>
+
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<?php
+include("includes/config.php");
+
+// session_destroy();  Logout
+
+if(isset($_SESSION['userLoggedIn'])) {
+  $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+  header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Welcome to Slotify</title>
+</head>
+<body>
+
+  <div id="mainContainer">
+
+    <div id="topContainer">
+
+      <?php include("includes/navBarContainer.php"); ?>
+
+      <div id="mainViewContainer">
+
+        <div id="mainContent">
