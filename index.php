ec02ce357d1e996b1df413f9651e6ec12803b5d1
Edwin Chen <edwinchen85@gmail.com>
2018-05-20 13:05:16 +0800

    Add markup and style for "nowPlayingLeft", "nowPlayingCenter" and "nowPlayingRight"

--- index.php
+++ index.php
@@ -27,6 +27,18 @@ else {
 
     <div id="nowPlayingBar">
 
+      <div id="nowPlayingLeft">
+
+      </div>
+
+      <div id="nowPlayingCenter">
+
+      </div>
+
+      <div id="nowPlayingRight">
+
+      </div>
+
     </div>
 
   </div>
