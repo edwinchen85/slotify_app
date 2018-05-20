3c47242f00ac817366e6b59b195eea04a6f7e472
Edwin Chen <edwinchen85@gmail.com>
2018-05-20 11:39:53 +0800

    Add markup for right section content

--- register.php
+++ register.php
@@ -114,6 +114,16 @@
 
       </div>
 
+      <div id="loginText">
+        <h1>Get great music, right now</h1>
+        <h2>Listen to lots of songs for free</h2>
+        <ul>
+          <li>Discover music you'll fall in love with</li>
+          <li>Create your own playlists</li>
+          <li>Follow artists to keep up to date</li>
+        </ul>
+      </div>
+
     </div>
 
   </div>
