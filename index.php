5fe172f9f3eea6aa33f5496cce6c8f898498ede2
Edwin Chen <edwinchen85@gmail.com>
2018-05-20 14:30:40 +0800

    Add markup and style nav search icon
    Correct

--- index.php
+++ index.php
@@ -36,7 +36,9 @@ else {
 
           <div class="group">
             <div class="navItem">
-              <a href="search.php" class="navItemLink">Search</a>
+              <a href="search.php" class="navItemLink">Search
+                <img src="assets/images/icons/search.png" class="icon" alt="Search">
+              </a>
             </div>
           </div>
 
