88f9ca31e12043cc4dd34021acc4637c30ac1c68
Edwin Chen <edwinchen85@gmail.com>
2018-05-22 07:51:40 +0800

    Update play counts when a song is played

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -38,8 +38,8 @@ $jsonArray = json_encode($resultArray);
         $(".albumLink img").attr("src", album.artworkPath);
       });
 
-      audioElement.setTrack(track.path);
-      audioElement.play();
+      audioElement.setTrack(track);
+      playSong();
     });
 
 
@@ -49,6 +49,10 @@ $jsonArray = json_encode($resultArray);
   }
 
   function playSong() {
+    if(audioElement.audio.currentTime == 0) {
+      $.post("includes/handlers/ajax/updatePlays.php", { songId: audioElement.currentlyPlaying.id });
+    }
+
     $(".controlButton.play").hide();
     $(".controlButton.pause").show();
     audioElement.play();
