b94113002b3c4c500e6439b9938a9b530c45402a
Edwin Chen <edwinchen85@gmail.com>
2018-05-22 21:51:44 +0800

    Hook up set repeat to repeat button

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -90,6 +90,12 @@ $jsonArray = json_encode($resultArray);
     setTrack(trackToPlay, currentPlaylist, true);
   }
 
+  function setRepeat() {
+    repeat = !repeat;
+    var imageName = repeat ? "repeat-active.png" : "repeat.png";
+    $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
+  }
+
   function setTrack(trackId, newPlaylist, play) {
 
     var currentIndex = currentPlaylist.indexOf(trackId);
@@ -186,7 +192,7 @@ $jsonArray = json_encode($resultArray);
           <img src="assets/images/icons/next.png" alt="Next">
         </button>
 
-        <button class="controlButton repeat" title="Repeat button">
+        <button class="controlButton repeat" title="Repeat button" onclick="setRepeat()">
           <img src="assets/images/icons/repeat.png" alt="Repeat">
         </button>
 
