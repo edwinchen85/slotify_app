dd1137fe2a7d7dc9f0abcff0dc976a92f67551fd
Edwin Chen <edwinchen85@gmail.com>
2018-05-22 21:39:16 +0800

    Skip to next song

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -72,8 +72,22 @@ $jsonArray = json_encode($resultArray);
     audioElement.setTime(seconds);
   }
 
+  function nextSong() {
+    if(currentIndex == currentPlaylist.length - 1) {
+      currentIndex = 0;
+    }
+    else {
+      currentIndex++;
+    }
+
+    var trackToPlay = currentPlaylist[currentIndex];
+    setTrack(trackToPlay, currentPlaylist, true);
+  }
+
   function setTrack(trackId, newPlaylist, play) {
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
+
+      var currentIndex = currentPlaylist.indexOf(trackId);
       var track = JSON.parse(data);
 
       $(".trackName span").text(track.title);
@@ -159,7 +173,7 @@ $jsonArray = json_encode($resultArray);
           <img src="assets/images/icons/pause.png" alt="Pause">
         </button>
 
-        <button class="controlButton next" title="Next button">
+        <button class="controlButton next" title="Next button" onclick="nextSong()">
           <img src="assets/images/icons/next.png" alt="Next">
         </button>
 
