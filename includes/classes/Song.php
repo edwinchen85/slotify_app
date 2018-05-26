f5d9a94d61b29000c35d8f2429ce2ebfc0acac16
Edwin Chen <edwinchen85@gmail.com>
2018-05-26 20:38:25 +0800

    Play songs by clicking on the song's play button

--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -24,6 +24,11 @@
       $this->path = $this->mysqliData['path'];
     }
 
+    public function getId()
+    {
+      return $this->id;
+    }
+
     public function getTitle()
     {
       return $this->title;
